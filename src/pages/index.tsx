2a75f69fa20c9f8d3d85428e0b037350f44b950f
tomon9086 <tomon9086@gmail.com>
2023-08-31 18:13:46 +0900

    list doc files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import { readDocPaths } from '@/api/file'
 import DefaultLayout from '@/layouts/default'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 
-const HomePage = () => {
+type StaticProps = {
+  docs: string[]
+}
+
+const HomePage = ({ docs }: StaticProps) => {
   return (
     <>
       <Head>
@@ -11,12 +17,26 @@ const HomePage = () => {
       </Head>
 
       <DefaultLayout>
-        <div>
-          <Link href='hello/world'>hello/world</Link>
-        </div>
+        <ul>
+          {docs.map((path) => (
+            <li key={path}>
+              <Link href={path}>{path}</Link>
+            </li>
+          ))}
+        </ul>
       </DefaultLayout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+  const docs = await readDocPaths()
+
+  return {
+    props: {
+      docs
+    }
+  }
 }
 
 export default HomePage
