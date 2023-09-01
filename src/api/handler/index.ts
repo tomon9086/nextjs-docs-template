4c962d3863d630ffb3b0f35949af56b0478bfffe
tomon9086 <tomon9086@gmail.com>
2023-09-01 11:16:18 +0900

    remove unused modules & packages

--- src/api/handler/index.ts
+++
@@ -1,4 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import nc from 'next-connect'
-
-export const handler = () => nc<NextApiRequest, NextApiResponse>()
--- src/pages/api/ping/index.ts
+++
@@ -1,5 +0,0 @@
-import { handler } from '@/api/handler'
-
-export default handler().get((_, res) => {
-  res.send('pong')
-})
